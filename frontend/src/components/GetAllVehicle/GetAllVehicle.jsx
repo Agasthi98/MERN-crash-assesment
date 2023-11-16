18dcb399f89ce872ecb5cd912fd0782b6b677dd8
Agasthi98 <sankalana98@gmail.com>
2023-11-16 16:30:48 +0530

    add success

--- frontend/src/components/GetAllVehicle/GetAllVehicle.jsx
+++ frontend/src/components/GetAllVehicle/GetAllVehicle.jsx
@@ -11,6 +11,7 @@ const GetAllVehicle = ({ data }) => {
               <th scope="col">Owners</th>
               <th scope="col">Price</th>
               <th scope="col">Color</th>
+              <th scope="col">Action</th>
             </tr>
           </thead>
           <tbody>
@@ -22,6 +23,25 @@ const GetAllVehicle = ({ data }) => {
                 <td>{item.owners}</td>
                 <td>{item.price}</td>
                 <td>{item.color}</td>
+                <td>
+                  {" "}
+                  <button type="button" className="btn btn-success ">
+                    Edit
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-danger"
+                    style={{ marginLeft: "10px" }}
+                  >
+                    Delete
+                  </button>
+                </td>
+                {/* <td>
+                  {" "}
+                  <button type="button" className="btn btn-danger">
+                    Delete
+                  </button>
+                </td> */}
               </tr>
             ))}
           </tbody>
